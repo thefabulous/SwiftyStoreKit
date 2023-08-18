20e295d1f78616784af1b8408b514a2ab6031221
Jaroslaw Gajak <jaroslaw.gajak@thefabulous.co>
2023-08-18 16:09:51 +0200

    Making retrieveProductsInfo thread safe by changing standard dictionary to a thread-safe one.

--- Sources/SwiftyStoreKit/ProductsInfoController.swift
+++ Sources/SwiftyStoreKit/ProductsInfoController.swift
@@ -49,7 +49,7 @@ class ProductsInfoController: NSObject {
     }
     
     // As we can have multiple inflight requests, we store them in a dictionary by product ids
-    private var inflightRequests: [Set<String>: InAppProductQuery] = [:]
+    private var inflightRequests = ThreadSafeDictionary<Set<String>, InAppProductQuery>()
 
     @discardableResult
     func retrieveProductsInfo(_ productIds: Set<String>, completion: @escaping (RetrieveResults) -> Void) -> InAppProductRequest {
